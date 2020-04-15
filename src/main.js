2280d46ae7877e08a708d3b580ae20e875a1f92c
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-15 16:04:35 -0700

    update javascript functions and css

--- src/main.js
+++ src/main.js
@@ -1,17 +1,28 @@
-import $ from 'jquery'; 
-import 'bootstrap'
+import $ from 'jquery';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { myDate } from './weekday';
 
-$(document).ready(function() {
-  $('#dateInput').submit(function(event) {
+$(document).ready(function () {
+  $('#date-input').submit(function (event) {
     event.preventDefault();
 
-   let inputYear = parseInt($('#year').val());
-   let inputMonth = parseInt($("#month").val());
-   let inputDay = parseInt($("#day").val());
+    let inputYear = parseInt($("#year").val());
+    let inputMonth = parseInt($("#month").val());
+    console.log(inputMonth);
+    let inputDay = parseInt($("#day").val());
+    console.log(inputDay);
 
-   let inputCalculator = new Date (inputYear, inputMonth, inputDay);
-   $("#result").text(inputCalculator.datecheck());
+    let dayofWeek = new myDate(inputMonth, inputDay, inputYear);
+    console.log(dayofWeek);
+    let day = dayofWeek.dateCheck();
+    $("#showresult").html("<p>" + "This date falls on a" + day + "." + "</p>");
+    $("#result").show();
+  });
 });
-});
+
+
+
+
+//  $("#showresult").html(inputCalculator.datecheck()); .html('<p>' + 'the weekday is/was' + weekDay + '.' + '</p>') 
--- src/weekday.js
+++ src/weekday.js
@@ -1,33 +1,34 @@
-export function Date (month, day, year) {
+export function myDate(month, day, year) {
   this.month = month;
   this.day = day;
   this.year = year;
 
 }
 
-Date.prototype.dateCheck = function () {
-  var daysofWeek = new Array (7);
-    daysofWeek[0] = "Saturday";
-    daysofWeek[1] = "Sunday";
-    daysofWeek[2] = "Monday";
-    daysofWeek[3] = "Tuesday";
-    daysofWeek[4] = "Wednesday";
-    daysofWeek[5] = "Thursday";
-    daysofWeek[6] = "Friday";
+myDate.prototype.dateCheck = function () {
+  var daysofWeek = new Array(7);
+  daysofWeek[0] = "Saturday";
+  daysofWeek[1] = "Sunday";
+  daysofWeek[2] = "Monday";
+  daysofWeek[3] = "Tuesday";
+  daysofWeek[4] = "Wednesday";
+  daysofWeek[5] = "Thursday";
+  daysofWeek[6] = "Friday";
 
-    let date = new Date("${this.day}, ${this.month},${this.year}")
-    if (date == "Not a valid date") {
-      return false
-    } else {
+  // let date = new Date("${this.day}, ${this.month}, ${this.year}");
+  let date = new Date(this.day, this.month, this.year)
+  if (date == "Not a valid date") {
+    return false
+  } else {
 
-      let result = daysofWeek[date.showDay()]
-      return result
-    }
+    let resultDate = daysofWeek[date.showDay()]
+    return resultDate
   }
+}
 
 
 
-  
+
    // if (this.month > 12) {
   //   return "not valid month";
   // } else if (this.month === 1 || this.month === 3 || this.month === 5 || this.month === 7 || this.month === 8 || this.month === 10 || this.month === 12) {
